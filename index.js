b58df47fe053fdc4dd4de9bdbedc685329c6cc01
cerna234 <cerna2424@gmail.com>
2022-05-28 17:19:50 -0700

    added axios for api and font from google fonts

--- index.js
+++ index.js
@@ -1,31 +1,89 @@
 
 
-var request = new XMLHttpRequest()
+///////////////////
 
 let input = document.getElementById("inputValue");
 let output = document.getElementById("output");
 let currency = document.getElementById("currency");
 let symbol = document.getElementById("symbol");
+let Switch = document.getElementById("Switch");
 
-request.open('GET', "https://api.coindesk.com/v1/bpi/currentprice.json", true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-
-  if (request.status >= 200 && request.status < 400) {
-
-     let UsRate = data.bpi.USD.rate_float;
-     let UeropeRate = data.bpi.EUR.rate_float;
-     let BritishPoundRate = data.bpi.GBP.rate_float;
+let trigger = true;
 
 
-     let submitButton = document.getElementById("submitButton");
+const price = async () => {
 
 
-     currency.addEventListener("change", (e)=> {
-             e.preventDefault();
+        try{
+                const config = {headers: {Accept: `application/json`}}
+                const res = await axios.get("https://api.coindesk.com/v1/bpi/currentprice.json", config);
+                const data = res.data.bpi;
+            
+                let UsRate = data.USD.rate_float;
+                let UeropeRate = data.EUR.rate_float;
+                let BritishPoundRate = data.GBP.rate_float;
+                
 
-             if(currency.value === "USD"){
+                let submitButton = document.getElementById("submitButton");
+               
+
+
+                
+
+                submitButton.addEventListener("click", (e) => {
+                        e.preventDefault();
+           
+                      
+           
+                        let chosenCurrency;
+                        let monetarySymbol;
+           
+                        if(input.value === ""){
+                                console.log("wrong");
+                                input.style.border ="2px solid red";
+                                output.innerHTML = "Enter Value"
+                        }else{
+           
+                           input.style.border ="none";
+                           if(currency.value === "USD"){
+                                   chosenCurrency = UsRate;
+                                   symbol.src = "us.png";
+                                   monetarySymbol="$";
+                           }else if(currency.value === "EUR"){
+                                   chosenCurrency = UeropeRate;
+                                   symbol.src = "europe.png";
+                                   monetarySymbol = "&euro";
+                           }else{
+                                   chosenCurrency = BritishPoundRate;
+                                   symbol.src = "British.png";
+                                   monetarySymbol = "&#163"
+                           }
+                           input.style.border = "  1px solid rgba(206, 205, 205, 0.322)"
+                    
+                             
+                           output.innerHTML = monetarySymbol + " " + input.value + " = "  + (input.value / chosenCurrency) + " &#8383"; 
+                          
+                          
+                                
+                        }
+           
+                      
+           
+                })
+                
+               
+        }catch(e){
+                console.log(e)
+        }
+      
+       
+
+}
+
+currency.addEventListener("change", (e)=> {
+        e.preventDefault();
+
+        if(currency.value === "USD"){
 
                 symbol.src = "us.png";
         }else if(currency.value === "EUR"){
@@ -36,64 +94,10 @@ request.onload = function () {
                 symbol.src = "British.png";
         }
 
-     })
-
-     submitButton.addEventListener("click", (e) => {
-             e.preventDefault();
-
-           
-
-             let chosenCurrency;
-             let monetarySymbol;
-
-             if(input.value === ""){
-                     console.log("wrong");
-                     input.style.border ="2px solid red";
-                     output.innerHTML = "Enter Value"
-             }else{
-
-                input.style.border ="none";
-                if(currency.value === "USD"){
-                        chosenCurrency = UsRate;
-                        symbol.src = "us.png";
-                        monetarySymbol="$";
-                }else if(currency.value === "EUR"){
-                        chosenCurrency = UeropeRate;
-                        symbol.src = "europe.png";
-                        monetarySymbol = "&euro";
-                }else{
-                        chosenCurrency = BritishPoundRate;
-                        symbol.src = "British.png";
-                        monetarySymbol = "&#163"
-                }
-                input.style.border = "  1px solid rgba(206, 205, 205, 0.322)"
-         
-                  
-                output.innerHTML = monetarySymbol + " " + input.value + " = "  +  (input.value / chosenCurrency) + " &#8383"; 
-   
-               
-                     
-             }
-
-           
-
-     })
-    
+})
 
 
 
 
-  } else {
-    console.log('error')
-  }
 
-  
-}
-
-
-
-
-request.send()
-
-
-
+price();
